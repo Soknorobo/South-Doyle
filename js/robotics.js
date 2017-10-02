71d8bf0bbf2e54486c14091595ddf5c1908f2017
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2017-10-01 23:49:13 -0400

    preload test

--- js/robotics.js
+++ js/robotics.js
@@ -48,67 +48,6 @@ $(document).ready(function() {
 });
 
 
-
-
-// gallery slideshow
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-// prev and next button function
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-document.addEventListener("keyup",function(e){
-   var key = e.which||e.keyCode;
-   switch(key){
-      //left arrow
-      case 37:
-         document.getElementById("prev").click();
-      break;
-      //right arrow
-      case 39:
-         document.getElementById("next").click();
-      break;
-   }
-});
-
-
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-}
-
 // touch
 //
 //
