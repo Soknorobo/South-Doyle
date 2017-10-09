a5577ebfa17b812bf64d675bdae2a89cebfa7e4e
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2017-10-08 22:01:59 -0400

    1.17
    optimized gallery
    optimized header
    workingn on LQIP

--- js/robotics.js
+++ js/robotics.js
@@ -19,37 +19,44 @@ $(document).ready(function() {
 // $("header").addClass("shrink");
 
 $(document).on("scroll", function() {
-  if ($(document).scrollTop() > 100) {
+  // when scrolled 5 lines down
+  if ($(document).scrollTop() > 5) {
+    // the logo resizes to fit in the smaller header
     document.getElementById("logo").style.fontSize = "40px";
+    // the header changes color from radient to blue
     document.getElementById("heading").style.background = "#303C88";
+    // makes the header transparent
     // document.getElementById("heading").style.opacity = ".9";
+    // removes the logo shadow
     document.getElementById("logo").style.textShadow = "none";
+    // modes the hamburgercross to center on new header
     document.getElementById("hamburger").style.transform = "translateY(-5px)";
+    // moving the logo to he left
     // document.getElementById("logo").style.textAlign = "left";
-  } else if ($(document).scrollTop() < 5) {
+  } else {
+    // for the default posiitons
     document.getElementById("logo").style.fontSize = "60px";
     document.getElementById("heading").style.background = "inherit";
     document.getElementById("logo").style.textShadow = "inherit";
-  } else {
-    // document.getElementById("heading").style.opacity = "1";
     document.getElementById("hamburger").style.transform = "translateY(0px)";
+    // document.getElementById("heading").style.opacity = "1";
     // document.getElementById("logo").style.textAlign = "center";
   }
 });
 
 // the sidebar
 $(document).ready(function() {
+  // when clicked on hamburger opens or closes overlay
   var trigger = $('.hamburger'),
     overlay = $('.overlay'),
     isClosed = false;
 
-
+  // hamburger_cross
   trigger.click(function() {
     hamburger_cross();
   });
 
   function hamburger_cross() {
-
     if (isClosed == true) {
       overlay.hide();
       trigger.removeClass('is-open');
@@ -67,3 +74,29 @@ $(document).ready(function() {
     $('#wrapper').toggleClass('toggled');
   });
 });
+
+
+
+// LQIP
+// find element
+const image = document.getElementsByTagName("img")[0];
+// remove no-js attribute as we know js has been exectuted
+image.removeAttribute("no-js");
+// grab the image src before we rewrite it
+const fullSrc = image.src;
+// rewrite the image src with the placeholder base64 string
+image.src = image.getAttribute("placeholder");
+// create a new image
+const fullImage = new Image();
+// assign the saved src to the new image to preload it
+fullImage.src = fullSrc;
+// wait for the full image to load
+fullImage.onload = function() {
+  // add a small timeout to allow the transition when the image is already in memory
+  setTimeout(() => {
+    // replace the placeholder src with the full image src
+    image.src = fullSrc;
+    // remove the placeholder attribute to trigger the css transition
+    image.removeAttribute("placeholder");
+  }, 200);
+};
