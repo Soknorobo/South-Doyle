5bbb9f2231a808a51e05470fa0a6bcca3ca3d86e
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2017-11-08 00:38:29 -0500

    1.9.3
    looooodddding bar

--- js/robotics.js
+++ js/robotics.js
@@ -1,49 +1,17 @@
-//
-// ?
-// This contains all the extra javascript
-// ?
-
-
-
-
-// tooltip javascript, creates tooltip
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
-
-
-// adjust the carousel speed
-// // carousel speed javascript
-// $(document).ready(function() {
-//     $('.carousel').carousel({
-//       interval: 1500
-//     })
-//   });
-
-// header
-// $("header").addClass("shrink");
-
 $(document).on("scroll", function() {
-  // when scrolled 5 lines down
   if ($(document).scrollTop() > 0) {
-    // the logo resizes to fit in the smaller header
     document.getElementById("logo").style.fontSize = "35px";
     document.getElementById("logo").style.padding = "4px 0px 0px 0px";
-    // the header changes color from radient to blue
     document.getElementById("heading").style.background = "#303C88";
     document.getElementById("heading").style.padding = "0";
     document.getElementById("login").style.padding = "5px 10px";
     document.getElementById("upload").style.padding = "5px 10px";
-    // makes the header transparent
-    // document.getElementById("heading").style.opacity = ".9";
-    // removes the logo shadow
     document.getElementById("logo-sp").style.textShadow = "none";
-    // modes the hamburgercross to center on new header
     document.getElementById("hamburger").style.transform = "translateY(-12px)";
-    // moving the logo to he left
-    // document.getElementById("logo").style.textAlign = "left";
   } else {
-    // for the default posiitons
     document.getElementById("logo").style.fontSize = "40px";
     document.getElementById("logo").style.padding = "none";
     document.getElementById("heading").style.background = "inherit";
@@ -52,24 +20,15 @@ $(document).on("scroll", function() {
     document.getElementById("upload").style.padding = "10px 15px";
     document.getElementById("logo-sp").style.textShadow = "inherit";
     document.getElementById("hamburger").style.transform = "translateY(0px)";
-    // document.getElementById("heading").style.opacity = "1";
-    // document.getElementById("logo").style.textAlign = "center";
   }
 });
-
-
-// the sidebar
 $(document).ready(function() {
-  // when clicked on hamburger opens or closes overlay
   var trigger = $('.hamburger'),
     overlay = $('.overlay'),
     isClosed = false;
-
-  // hamburger_cross
   trigger.click(function() {
     hamburger_cross();
   });
-
   function hamburger_cross() {
     if (isClosed == true) {
       overlay.hide();
@@ -83,14 +42,10 @@ $(document).ready(function() {
       isClosed = true;
     }
   }
-
   $('[data-toggle="offcanvas"]').click(function() {
     $('#wrapper').toggleClass('toggled');
   });
 });
-
-
-// LQIP LOW QUALITY IMAGE PLACEMENT
 function init() {
   var imgDefer = document.getElementsByTagName('img');
   for (var i = 0; i < imgDefer.length; i++) {
@@ -102,3 +57,18 @@ function init() {
   }
 }
 window.onload = init;
+$( document ).ready(function() {
+       $({property: 0}).animate({property: 105}, {
+           duration:2000,
+           step: function() {
+               var _percent = Math.round(this.property);
+               $('#progress').css('width',  _percent+"%");
+               if(_percent == 105) {
+                   $("#progress").addClass("done");
+               }
+           },
+           complete: function() {
+               $("#progress").hide();
+           }
+       });
+   });
