a6b27b60c875bf9a685f2b2b25aeb93360c1773d
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2017-11-08 22:45:02 -0500

    quick fix, the header should be fixed

--- js/robotics.js
+++ js/robotics.js
@@ -22,6 +22,7 @@ $(document).on("scroll", function() {
     document.getElementById("hamburger").style.transform = "translateY(0px)";
   }
 });
+
 $(document).ready(function() {
   var trigger = $('.hamburger'),
     overlay = $('.overlay'),
