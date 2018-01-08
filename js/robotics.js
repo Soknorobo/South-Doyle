a9dd5ea63c35f01f7a1f35a5c9bbf4ab85b87bec
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2018-01-08 14:53:05 -0500

    ...

--- js/robotics.js
+++ js/robotics.js
@@ -53,8 +53,6 @@ function init() {
   for (var i = 0; i < imgDefer.length; i++) {
     if (imgDefer[i].getAttribute('data-src')) {
       imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
-      imgDefer[i].style.animation = "unblur 1s ease";
-      imgDefer[i].style.filter = "blur(0px)";
     }
   }
 }
