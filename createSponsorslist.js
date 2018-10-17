59bc230007fc99009f90409efc0e07800b53c471
Atikul <atikul@Raspber6675636b.attlocal.net>
2018-10-16 22:04:45 -0400

    image renames

--- createSponsorslist.js
+++
@@ -1,9 +0,0 @@
-<div class="half d-md-flex d-block">
-    <div class="image" data-aos="fade" style="background-image: url('./img/sponsors/knoxschools.png');"></div>
-    <div class="text" data-aos="fade-right" data-aos-delay="200">
-        <h3>Knox County Schools</h3>
-        <p>Knox County Schools is the school district that operates all public schools in Knox County, Tennessee. It is headquartered in the Andrew Johnson Building in downtown Knoxville. It shall be the mission of the Knox County Schools Security Division to make the Knox County Schools a safe and secure learning environment, to assist students, staff, and administrators by placing them in the best possible position to be successful, and to cooperate with all entities and individuals engaged in the education of students.</p>
-
-        <p class="mt-5"><a href="https://www.knoxschools.org/" target="_blank" aria-hidden="true" class="btn btn-primary uppercase">Learn More</a></p>
-    </div>
-</div>
